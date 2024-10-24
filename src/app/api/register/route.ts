87b137e944b763d5b0123953c23643b9eae8e54a
Aglailson Cleber <aglailsonmail@gmail.com>
2024-10-24 14:34:45 -0300

    Corrigindo erros Unexpected any para build no vercel

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,15 +1,19 @@
-'use server';
-
-import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
 import { getUser, createUser } from '../../db';
 
-export async function register(name: string, email: string, password: string) {
-  const user = await getUser(email);
+export async function POST(request: Request) {
+  try {
+    const { name, email, password } = await request.json(); // Extrai os dados da requisição
 
-  if (user === null) {
-    await createUser(name, email, password);
-    redirect('/login');
-  } else {
-    return 'User already exists';
+    const user = await getUser(email);
+
+    if (user === null) {
+      await createUser(name, email, password);
+      return NextResponse.redirect('/login');
+    } else {
+      return NextResponse.json({ message: 'User already exists' });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
   }
 }
