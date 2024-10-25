0cf536eac30aa1be9bf4aae38824079108e09d45
Aglailson Cleber <aglailsonmail@gmail.com>
2024-10-25 09:40:10 -0300

    Correção funcionalidades Auth

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,12 +1,12 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compare } from 'bcrypt-ts';
 import { getUser } from './db';
 import { authConfig } from './auth.config';
 
-export const authOptions = NextAuth({
-  pages:{
-    signIn:"/",
+export const authOptions: AuthOptions = {
+  pages: {
+    signIn: "/",
   },
   ...authConfig,
   providers: [
@@ -17,14 +17,27 @@ export const authOptions = NextAuth({
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials) {
-        const { email, password } = credentials;
+        if (!credentials) return null;
+
+        const email = credentials.username;
+        const password = credentials.password;
+
         const user = await getUser(email);
         if (!user) return null;
-        const passwordsMatch = await compare(password, user.password!);
-        if (passwordsMatch) return user;
-        return null;
+
+        const passwordsMatch = await compare(password, user.password);
+        if (!passwordsMatch) return null;
+
+        // Ajustando para o formato esperado pelo NextAuth
+        return {
+          id: user._id.toString(),  // usando _id do MongoDB como ID de usuário
+          email: user.email,
+          name: user.name,          // opcional, mas você pode adicionar se estiver disponível
+        };
       }
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+};
+
+export default NextAuth(authOptions);
